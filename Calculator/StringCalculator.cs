0cc6f0680e139baa7b5f9e0154a2e1db06daedd2
SRAVAN <SRAVAN@DESKTOP-2169NFB>
2019-08-22 23:01:42 +0100

    Added first test

---
+++ Calculator/StringCalculator.cs
@@ -0,0 +1,34 @@
+﻿namespace NU.Calculator
+{
+    public interface IStringCalculator
+    {
+        int Add(string input);
+    }
+
+    public class StringCalculator : IStringCalculator
+    {
+        private const int DefaultValue = 0;
+
+        public StringCalculator()
+        {
+
+        }
+
+        public int Add(string input)
+        {
+            var result = 0;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return DefaultValue;
+            }
+
+            foreach (string number in input.Split(','))
+            {
+                result = result + int.Parse(number);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Calculator/StringCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NU.Calculator
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        private IStringCalculator stringCalculator;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            stringCalculator = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void return_zero_if_input_is_empty()
+        {
+            int expectedResult = 0;
+
+            int result = stringCalculator.Add("");
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
